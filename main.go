8e49d6d08a589a61ae7c5b4a438b35afa7c5965b
junix <junix.choi@gmail.com>
2022-05-23 23:26:48 +0900

    Gorilla Mux 라우터 적용

--- main.go
+++ main.go
@@ -3,17 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
+
+	"github.com/gorilla/mux"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, Goblog</h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>잘못된 경로입니다</h1>")
-	}
+	fmt.Fprint(w, "<h1>Hello, Goblog</h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,24 +17,42 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "about page")
 }
 
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>잘못된 경로입니다</h1>")
+}
+
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprint(w, "Article ID: "+id)
+}
+
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "articles list")
+}
+
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "new article")
+}
+
 func main() {
-	router := http.NewServeMux()
-	router.HandleFunc("/", defaultHandler)
-	router.HandleFunc("/about", aboutHandler)
+	router := mux.NewRouter()
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
-	router.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
-		id := strings.SplitN(r.URL.Path, "/", 3)[2]
-		fmt.Fprint(w, "Article ID: "+id)
-	})
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", articlesShowHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesShowHandler).Methods("POST").Name("articles.store")
 
-	router.HandleFunc("/articles", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "GET":
-			fmt.Fprint(w, "articles list")
-		case "POST":
-			fmt.Fprint(w, "new article")
-		}
-	})
+	// 404 페이지
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	homeURL, _ := router.Get("home").URL()
+	fmt.Println("homeURL: ", homeURL)
+	articleURL, _ := router.Get("articles.show").URL("id", "23")
+	fmt.Println("articleURL: ", articleURL)
 
 	http.ListenAndServe(":3000", router)
 }

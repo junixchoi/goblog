85b724998d6ab46b2366c7a7916a85daba6b13f0
junix <junix.choi@gmail.com>
2022-05-23 22:52:49 +0900

    about 페이지 추가

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> Hello, Goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, Goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "about page")
+	} else {
+		fmt.Fprint(w, "<h1>잘못된 경로입니다</h1>")
+	}
 }
 
 func main() {

e4a96af8db82a687654cb71c5df3fe564efe2f5f
junix <junix.choi@gmail.com>
2022-05-25 22:24:49 +0900

    form validation

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"net/url"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/gorilla/mux"
 )
@@ -32,11 +35,78 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "articles list")
 }
 
+type ArticlesFormData struct {
+	Title, Body string
+	URL         *url.URL
+	Errors      map[string]string
+}
+
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "in r.Form, title is: %v <br>", r.FormValue("title"))
-	fmt.Fprintf(w, "in r.PostForm, title is: %v <br>", r.PostFormValue("title"))
-	fmt.Fprintf(w, "in r.Form, test is: %v <br>", r.FormValue("test"))
-	fmt.Fprintf(w, "in r.PostForm, test is: %v <br>", r.PostFormValue("test"))
+	title := r.PostFormValue("title")
+	body := r.PostFormValue("body")
+	errors := make(map[string]string)
+
+	// title validator
+	if title == "" {
+		errors["title"] = "title is empty"
+	} else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
+		errors["title"] = "title's length is > 3, < 40"
+	}
+
+	// body validator
+	if body == "" {
+		errors["body"] = "body is empty"
+	} else if utf8.RuneCountInString(body) < 10 {
+		errors["body"] = "body's length must >= 10"
+	}
+
+	if len(errors) == 0 {
+		fmt.Fprintf(w, "validated! <br>")
+		fmt.Fprintf(w, "title is: %v <br>", title)
+		fmt.Fprintf(w, "title's length is: %v <br>", utf8.RuneCountInString(title))
+		fmt.Fprintf(w, "body is: %v <br>", body)
+		fmt.Fprintf(w, "body's length is: %v <br>", utf8.RuneCountInString(body))
+	} else {
+		html := `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+		<title>my go blog</title>
+		<style type="text/css">.error {color: red;}</style>
+		</head>
+		<body>
+			<form action="{{ .URL }}" method="post">
+				<p><input type="text" name="title" value="{{ .Title }}"></p>
+				{{ with .Errors.title }}
+				<p class="error">{{ . }}</p>
+				{{ end }}
+				<p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
+				{{ with .Errors.body }}
+				<p class="error">{{ . }}</p>
+				{{ end }}
+				<p><button type="submit">Submit</button></p>
+			</form>
+		</body>
+		</html>
+		`
+		storeURL, _ := router.Get("articles.store").URL()
+
+		data := ArticlesFormData{
+			Title:  title,
+			Body:   body,
+			URL:    storeURL,
+			Errors: errors,
+		}
+		tmpl, err := template.New("create-form").Parse(html)
+		if err != nil {
+			panic(err)
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func forceHTMLMiddleware(next http.Handler) http.Handler {

91ca14170a14f59261715eece819a902cb020545
junix <junix.choi@gmail.com>
2022-06-06 00:51:20 +0900

    finish code tests

--- tests/pages_test.go
+++ tests/pages_test.go
@@ -3,18 +3,47 @@ package tests
 import (
 	"github.com/stretchr/testify/assert"
 	"net/http"
+	"strconv"
 	"testing"
 )
 
-func TestHomePage(t *testing.T) {
+func TestAllPage(t *testing.T) {
 	baseURL := "http://localhost:3000"
 
-	var (
-		resp *http.Response
-		err  error
-	)
-	resp, err = http.Get(baseURL + "/")
+	// init test data
+	var tests = []struct {
+		method   string
+		url      string
+		expected int
+	}{
+		{"GET", "/", 200},
+		{"GET", "/about", 200},
+		{"GET", "/notfound", 200},
+		{"GET", "/articles", 200},
+		{"GET", "/articles/create", 200},
+		{"GET", "/articles/3", 200},
+		{"GET", "/articles/3/edit", 200},
+		{"POST", "/articles/3", 200},
+		{"POST", "/articles", 200},
+		{"POST", "/articles/1/delete", 404},
+	}
 
-	assert.NoError(t, err, "err is empty")
-	assert.Equal(t, 200, resp.StatusCode, "must return 200 status code")
+	// 2. loop all tests
+	for _, test := range tests {
+		t.Logf("current request URL: %v \n", test.url)
+		var (
+			resp *http.Response
+			err  error
+		)
+		// 2.1 request and get response
+		switch {
+		case test.method == "POST":
+			data := make(map[string][]string)
+			resp, err = http.PostForm(baseURL+test.url, data)
+		default:
+			resp, err = http.Get(baseURL + test.url)
+		}
+		assert.NoError(t, err, "request "+test.url+" error")
+		assert.Equal(t, test.expected, resp.StatusCode, test.url+" must return status code "+strconv.Itoa(test.expected))
+	}
 }

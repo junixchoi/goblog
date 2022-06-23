4f610e86302e35e368ec45e887088ca616a05cea
junix <junix.choi@gmail.com>
2022-06-24 06:28:33 +0900

    article show page

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -38,15 +38,24 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "500 Error")
 		}
 	} else {
-		// 데이터 조회 성공
+		viewDir := "resources/views"
+
+		files, err := filepath.Glob(viewDir + "/layouts/*.gohtml")
+		logger.LogError(err)
+
+		newFiles := append(files, viewDir+"/articles/show.gohtml")
+
+		// parse template file
 		tmpl, err := template.New("show.gohtml").
 			Funcs(template.FuncMap{
 				"RouteName2URL":  route.Name2URL,
 				"Uint64ToString": types.Uint64ToString,
 			}).
-			ParseFiles("resources/views/articles/show.gohtml")
+			ParseFiles(newFiles...)
 		logger.LogError(err)
-		tmpl.Execute(w, article)
+
+		err = tmpl.ExecuteTemplate(w, "app", article)
+		logger.LogError(err)
 	}
 }
 
